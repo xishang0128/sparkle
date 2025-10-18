dbc77bb1d6fc852cd8834fc2113129e5c737ab5c
xishang0128 <xishang02@gmail.com>
2025-10-18 21:44:28 +0800

    Add a custom indicator to the SettingCard component

--- src/renderer/src/components/base/base-setting-card.tsx
+++ src/renderer/src/components/base/base-setting-card.tsx
@@ -16,9 +16,26 @@ const SettingCard: React.FC<Props> = (props) => {
     <Accordion isCompact className={`${props.className} my-2`} variant="splitted" {...props}>
       <AccordionItem
         className="data-[open=true]:pb-2"
-        hideIndicator
         keepContentMounted
         title={props.title}
+        indicator={({ isOpen }) => (
+          <svg
+            className={`transition-transform duration-200 ${isOpen ? 'rotate-180' : ''}`}
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M4 6L8 10L12 6"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        )}
       >
         {props.children}
       </AccordionItem>
