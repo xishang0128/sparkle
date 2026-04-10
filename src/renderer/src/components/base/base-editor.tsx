f9de7f081340233faef695a6119ca454808e544d
xishang0128 <xishang02@gmail.com>
2026-04-11 02:48:33 +0800

    migrate the config-viewer component to heroui v3

--- src/renderer/src/components/base/base-editor.tsx
+++ src/renderer/src/components/base/base-editor.tsx
@@ -103,13 +103,6 @@ export const BaseEditor: React.FC<Props> = (props) => {
   const hasLongLine = value.split('\n').some((line) => line.length > 5000) || value === ''
   const editorRef = useRef<monaco.editor.IStandaloneCodeEditor>(undefined)
   const diffEditorRef = useRef<monaco.editor.IStandaloneDiffEditor>(undefined)
-  console.log('BaseEditor render', {
-    language,
-    readOnly,
-    diffRenderSideBySide,
-    hasLongLine,
-    disableAnimation
-  })
 
   const editorWillMount = (): void => {
     monacoInitialization()
@@ -157,6 +150,7 @@ export const BaseEditor: React.FC<Props> = (props) => {
     smoothScrolling: !disableAnimation, // 禁用动画时关闭平滑滚动
     pixelRatio: window.devicePixelRatio, // 设置像素比
     renderSideBySide: diffRenderSideBySide, // 侧边显示
+    useInlineViewWhenSpaceIsLimited: false, // 侧边显示时不要自动退回内联模式
     glyphMargin: false, // 禁用字形边距
     folding: true, // 启用代码折叠
     scrollBeyondLastLine: false, // 禁止滚动超过最后一行
