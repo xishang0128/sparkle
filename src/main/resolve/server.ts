706278fedd19d30d4bdbbb9f3b150d146078bf89
xishang0128 <xishang02@gmail.com>
2026-09-24 05:04:55 +0800

    fix substore page refresh and dropdown styling

--- src/main/resolve/server.ts
+++ src/main/resolve/server.ts
@@ -122,7 +122,7 @@ export async function startSubStoreFrontendServer(): Promise<void> {
   const frontendDir = subStoreFrontendDir()
   app.use(express.static(frontendDir))
   app.use((_req, res) => {
-    res.sendFile(path.join(frontendDir, 'index.html'))
+    res.sendFile('index.html', { root: frontendDir })
   })
   subStoreFrontendServer = app.listen(subStoreFrontendPort, subStoreHost)
 }
